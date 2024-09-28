2f1cf748e386e0640f4a1a15554aba6e6b3f5665
Pietrell <piotrplotast2006@gmail.com>
2024-09-28 16:55:30 +0200

    add colors to config

--- age-calculator/tailwind.config.js
+++ age-calculator/tailwind.config.js
@@ -11,6 +11,18 @@ export default {
         poppinsExtraBold: ["poppins extra bold", "sans-serif"],
         poppinsExtraBoldItalic: ["poppins extra bold italic", "sans-serif"],
       },
+      colors: {
+        purple: "hsl(259,100%,65%)",
+        "light-red": "hsl(0, 100%, 67%)",
+        White: "hsl(0, 0%, 100%)",
+        "Off white": "hsl(0, 0%, 94%)",
+        "Light grey": "hsl(0, 0%, 86%)",
+        "Smokey grey": "hsl(0, 1%, 44%)",
+        "Off black": "hsl(0, 0%, 8%)",
+      },
+      fontSize: {
+        input: "2rem",
+      },
     },
   },
   plugins: [],
